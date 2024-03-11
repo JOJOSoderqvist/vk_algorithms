dfaa177d91c642b379f24030b9e9d5f923fb0701
jojosoderqvist <glebkarpikhin@gmail.com>
2024-03-11 23:06:22 +0300

    task_1 implemented

---
+++ module_1/task_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cassert>
+
+bool isBitNumberValid(int value, int bit_number){
+    int a = 1;
+    a <<= bit_number;
+    return value >= a;
+}
+
+int invertBit(int value, int bit_number){
+    if (value == 1)
+        return 0;
+
+    assert(isBitNumberValid(value, bit_number));
+    int inverted_bit;
+    inverted_bit = value >> bit_number;
+    inverted_bit ^= 1;
+
+    if (inverted_bit & 1){
+        inverted_bit <<= bit_number;
+        return value | inverted_bit;
+    } else {
+        inverted_bit <<= bit_number;
+        inverted_bit |= 1;
+        return inverted_bit & value;
+    }
+}
+
+int main(){
+    int value, bit_number;
+    std::cin >> value >> bit_number;
+    std::cout << invertBit(value, bit_number);
+}
