eb5444d8d4f1fe5d64734ecc9d49c1baccc56099
jojosoderqvist <glebkarpikhin@gmail.com>
2024-03-29 13:01:01 +0300

    fixed more format

--- module_1/include/Pair.hpp
+++ module_1/include/Pair.hpp
@@ -1,17 +1,17 @@
 #pragma once
 
-template<typename T1, typename T2>
-struct Pair{
+template <typename T1, typename T2>
+struct Pair {
     T1 _first;
     T2 _second;
 
     Pair() = default;
 
-    Pair(T1 first, T2 second){
+    Pair(T1 first, T2 second) {
         this->makePair(first, second);
     }
 
-    void makePair(T1 first, T2 second){
+    void makePair(T1 first, T2 second) {
         _first = first;
         _second = second;
     }
--- module_1/main.cpp
+++ module_1/main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
 #include "src/task_2_1.cpp"
 #include "src/task_3_1.cpp"
 #include "src/task_4_1.cpp"
+#include <iostream>
 
-int main(){
-  
+int main() {
 }
