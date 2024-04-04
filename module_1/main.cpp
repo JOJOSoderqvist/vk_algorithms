a191a7f369143b8fcfa273e042249b1161f466b6
jojosoderqvist <glebkarpikhin@gmail.com>
2024-04-05 02:33:14 +0300

    implemented task_6_2

--- module_1/main.cpp
+++ module_1/main.cpp
@@ -4,11 +4,7 @@
 #include "src/task_5_3.cpp"
 #include "src/task_6_2.cpp"
 #include <iostream>
-
-
-// bool customComparator(const int& a, const int& b) {
-//     return a > b;
-// }   
+  
 
 int main() {
     task_6_2::IO(std::cin, std::cout);
--- module_1/src/task_6_2.cpp
+++ module_1/src/task_6_2.cpp
@@ -1,59 +1,80 @@
-#include "../include/DynamicArray.hpp"
 #include <iostream>
 
 template <typename T>
 using comp_func = bool (*)(const T& l, const T& r);
 
 template <typename T>
-int partition(DynamicArray<T>& array, int left, int right, comp_func<T> comp) {
-    int pivot_idx = left + (right - left) / 2;
-    T& pivot = array[pivot_idx];
-    std::swap(array[pivot_idx], array[right - 1]);
-    int i = left;
-    int j = right - 1;
-    while (i < j) {
-        for (; comp(array[i], pivot); ++i);
-        for (; !comp(array[j], pivot); --j);
-
-        if (i < j){
-            std::swap(array[i], array[j]);
-            ++i;
-            --j;
-        }
-    }
-
-    std::swap(array[i], array[right - 1]);
-    return i;
+T min(T l, T r) {
+    return ((l == r) ? l : ((l < r) ? l : r));
 }
 
 template <typename T>
-void kth_element(DynamicArray<T>& array, int k, comp_func<T> comp) {
+T max(T l, T r) {
+    return ((l == r) ? l : ((l > r) ? l : r));
+}
+
+template <typename T>
+T median_of_three(T* array, int left, int right) {
+    int a = array[left];
+    int b = array[left + (right - left) / 2];
+    int c = array[right - 1];
+    int median = max(min(a, b), min(max(a, b), c));
+    return ((median == a) ? left : ((median == b) ? left + (right - left) / 2 : right - 1));
+}
+
+template <typename T>
+int partition(int* array, int left, int right, comp_func<T> comp) {
+    int pivot_idx = median_of_three(array, left, right);
+
+    std::swap(array[pivot_idx], array[left]);
+    T& pivot = array[left];
+    int j = right - 1;
+    int i = right;
+    while (j > left) {
+        if (!comp(array[j], pivot)) {
+            --i;
+            std::swap(array[i], array[j]);
+        }
+        --j;
+    }
+
+    std::swap(array[i - 1], array[left]);
+    return i - 1;
+}
+
+template <typename T>
+int kth_element(T* array, size_t size, int k, comp_func<T> comp) {
     int left = 0;
-    int right = array.getSize();
+    int right = size;
     while (left <= right) {
         int m = partition(array, left, right, comp);
-        if (m == k) 
-            return;
-        else if (k < m) 
+        if (m == k)
+            return array[m];
+        else if (k < m)
             right = m;
         else
             left = m + 1;
     }
 }
 
-bool defaultCompFunc(const int& l, const int& r){
+bool defaultCompFunc(const int& l, const int& r) {
     return l < r;
 }
 
 namespace task_6_2 {
     void IO(std::istream& input_stream, std::ostream& output_stream) {
-        DynamicArray<int> array;
-        array.pushBack(2);
-        array.pushBack(1);
-        array.pushBack(10);
-        array.pushBack(8);
-        array.pushBack(7);
+        int array_size;
+        input_stream >> array_size;
+        int array[array_size];
+        for (int i = 0; i < array_size; ++i)
+            input_stream >> array[i];
+        
 
-        kth_element(array, 7, defaultCompFunc);
+        int tenth_percent = static_cast<int>(array_size * 0.1f);
+        int ninetieth_percent = static_cast<int>(array_size * 0.9f);
+        int median = array_size / 2;
+        output_stream << kth_element(array, array_size, tenth_percent, defaultCompFunc) << '\n'
+                  << kth_element(array, array_size, median, defaultCompFunc) << '\n'
+                  << kth_element(array, array_size, ninetieth_percent, defaultCompFunc);
     }
 }
